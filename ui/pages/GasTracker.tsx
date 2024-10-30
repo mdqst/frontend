97e6f08b554846376feac71fc034ff79a2e4e084
isstuev <isstuev@gmail.com>
2024-10-30 22:43:03 +0100

    SEO Tweaks for Gas Tracker Page

--- ui/pages/GasTracker.tsx
+++ ui/pages/GasTracker.tsx
@@ -1,9 +1,23 @@
-import { Alert, Box, Flex, Skeleton, chakra } from '@chakra-ui/react';
+import {
+  Alert,
+  Box,
+  Flex,
+  Text,
+  Heading,
+  Skeleton,
+  Accordion,
+  chakra,
+  AccordionButton,
+  AccordionItem,
+  AccordionPanel,
+  AccordionIcon,
+} from '@chakra-ui/react';
 import React from 'react';
 
 import config from 'configs/app';
 import useApiQuery from 'lib/api/useApiQuery';
 import dayjs from 'lib/date/dayjs';
+import { currencyUnits } from 'lib/units';
 import { HOMEPAGE_STATS } from 'stubs/stats';
 import GasTrackerChart from 'ui/gasTracker/GasTrackerChart';
 import GasTrackerNetworkUtilization from 'ui/gasTracker/GasTrackerNetworkUtilization';
@@ -72,6 +86,60 @@ const GasTracker = () => {
     return data?.gas_prices ? <GasTrackerPrices prices={ data.gas_prices } isLoading={ isLoading }/> : null;
   })();
 
+  const faq = config.meta.seo.enhancedDataEnabled ? (
+    <Box mt={ 12 }>
+      <Heading as="h2" mb={ 4 } fontSize="2xl" fontWeight="medium">FAQ</Heading>
+      <Accordion>
+        <AccordionItem>
+          <h3>
+            <AccordionButton>
+              <Text flex="1" textAlign="left">What does gas refer to on the blockchain?</Text>
+              <AccordionIcon/>
+            </AccordionButton>
+          </h3>
+          <AccordionPanel>
+            <Text>Gas is the amount of native tokens required to perform a transaction on the blockchain.</Text>
+          </AccordionPanel>
+        </AccordionItem>
+        <AccordionItem>
+          <h3>
+            <AccordionButton>
+              <Text flex="1" textAlign="left">How can I check { config.chain.name } gas fees?</Text>
+              <AccordionIcon/>
+            </AccordionButton>
+          </h3>
+          <AccordionPanel>
+            { /* eslint-disable-next-line max-len */ }
+            <Text>You can easily check live { config.chain.name } gas fees on Blockscout by visiting our gas tracker. It displays current gas fees in { currencyUnits.gwei } for all { config.chain.name } transactions.</Text>
+          </AccordionPanel>
+        </AccordionItem>
+        <AccordionItem>
+          <h3>
+            <AccordionButton>
+              <Text flex="1" textAlign="left">What is the average gas fee for { config.chain.name } transactions?</Text>
+              <AccordionIcon/>
+            </AccordionButton>
+          </h3>
+          <AccordionPanel>
+            { /* eslint-disable-next-line max-len */ }
+            <Text>The average gas fee for { config.chain.name } transactions depends on network congestion and transaction complexity. Blockscout provides real-time gas fee estimations to help users make informed decisions.</Text>
+          </AccordionPanel>
+        </AccordionItem>
+        <AccordionItem>
+          <h3>
+            <AccordionButton>
+              <Text flex="1" textAlign="left">How does Blockscout calculate gas fees?</Text>
+              <AccordionIcon/>
+            </AccordionButton>
+          </h3>
+          <AccordionPanel>
+            <Text>Blockscout calculates gas fees based on the average price of gas fees spent for the last 200 blocks.</Text>
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
+    </Box>
+  ) : null;
+
   return (
     <>
       <PageTitle
@@ -79,12 +147,14 @@ const GasTracker = () => {
         secondRow={ titleSecondRow }
         withTextAd
       />
+      <Heading as="h2" mt={ 8 } mb={ 4 } fontSize="2xl">{ `Track ${ config.chain.name } gas fees` }</Heading>
       { snippets }
       { config.features.stats.isEnabled && (
         <Box mt={ 12 }>
           <GasTrackerChart/>
         </Box>
       ) }
+      { faq }
     </>
   );
 };
